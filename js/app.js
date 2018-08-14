878ae6e0b7309d69fef1b1a7301db0c051891224
Tracy Fussell <tracy.fussell@gmail.com>
2018-08-14 09:03:08 -0400

    Want to get bugs to move at random speeds.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 class Enemy {
-  constructor(){
+  constructor(x,y){
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    this.x = 0;
-    this.y= 50;
+    this.x = x;
+    this.y = y;
+    this.speed = this.x += Math.floor(Math.random() * 500) + 100; 
   }
     render() {
       ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
@@ -61,21 +62,22 @@ class Hero {
 let player = new Hero();
 
 // Enemies our player must avoid
-let roach1 = new Enemy();
+let roach1 = new Enemy(100,100);
+let roach2 = new Enemy(200,200);
+let roach3 = new Enemy(300,300);
 let allEnemies = [];
-allEnemies.push(roach1);
+allEnemies.push(roach1, roach2, roach3);
+
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
        if (this.x < 450) {
-         this.x +=500 * dt;
+         this.speed * dt;
        } else {
          this.x = -100;
        }
     }
-
-
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
@@ -106,3 +108,15 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+//
+// let roach1 = {radius: 20, x: 5, y: 5};
+// let player = {radius: 12, x: 10, y: 5};
+//
+// let dx = roach1.x - player.x;
+// let dy = roach1.y - player.y;
+// let distance = Math.sqrt(dx * dx + dy * dy);
+//
+// if (distance < circle1.radius + circle2.radius) {
+//     // collision detected!
+// }
